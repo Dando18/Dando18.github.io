adf959f65e189d8f5d54e5aa4ce416248392a53d
Daniel Nichols <dando18studios@gmail.com>
2020-04-03 12:02:29 -0400

    added roofline.js

---
+++ assets/js/posts/roofline.js
@@ -0,0 +1,15 @@
+let trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  type: 'scatter'
+};
+
+let trace2 = {
+  x: [1, 2, 3, 4],
+  y: [16, 5, 11, 9],
+  type: 'scatter'
+};
+
+let data = [trace1, trace2];
+
+Plotly.newPlot('roofline-plot', data);
