afda61a91e6724241e743aa308618dbcddb17d5d
Daniel Nichols <dando18studios@gmail.com>
2020-04-03 12:12:07 -0400

    wrapped in func

--- assets/js/posts/roofline.js
+++ assets/js/posts/roofline.js
@@ -1,15 +1,17 @@
-let trace1 = {
-  x: [1, 2, 3, 4],
-  y: [10, 15, 13, 17],
-  type: 'scatter'
-};
+function drawRooflinePlot() {
+  let trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    type: 'scatter'
+  };
 
-let trace2 = {
-  x: [1, 2, 3, 4],
-  y: [16, 5, 11, 9],
-  type: 'scatter'
-};
+  let trace2 = {
+    x: [1, 2, 3, 4],
+    y: [16, 5, 11, 9],
+    type: 'scatter'
+  };
 
-let data = [trace1, trace2];
+  let data = [trace1, trace2];
 
-Plotly.newPlot('roofline-plot', data);
+  Plotly.newPlot('roofline-plot', data);
+}
