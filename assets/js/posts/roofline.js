853e6c6a2e708e19f4168f2b5b1436fd2a4a732a
Daniel Nichols <dando18studios@gmail.com>
2020-04-04 10:08:44 -0400

    update plot stuff

--- assets/js/posts/roofline.js
+++ assets/js/posts/roofline.js
@@ -1,23 +1,46 @@
 function drawRooflinePlot() {
-  let trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
-    type: 'scatter'
+  const maxBandwith = 32;
+  const maxPerformance = 100;
+
+  let attainable = (ai, b, p) => { return Math.min(p, ai * b); }
+  let attainableLog = (ai, b, p) => { return Math.min(Math.log(p), Math.log(ai) + Math.log(b)); }
+
+  /* get lower X */
+  const x0 = 1.0 / 8.0;
+  const y0 = attainable(x0, maxBandwith, maxPerformance);
+
+  /* get turning point -- where band*ai = perf => ai = perf/bandwidth */
+  const x1 = maxPerformance / maxBandwith;
+  const y1 = attainableLog(x1, maxBandwith, maxPerformance);
+
+  /* get end of Roofline */
+  const x2 = Math.max(32, x1 + 1.0);
+  const y2 = attainableLog(x2, maxBandwith, maxPerformance);
+
+  let roofline = {
+    x: [x0, x1, x2],
+    y: [y0, y1, y2],
+    type: 'lines'
   };
 
-  let trace2 = {
-    x: [1, 2, 3, 4],
-    y: [16, 5, 11, 9],
-    type: 'scatter'
+  let data = [roofline];
+
+  let layout = {
+    title: 'Example Roofline Plot',
+    xaxis: {
+      type: 'log',
+      autorange: true,
+      title: 'Arithmetic Intensity',
+    },
+    yaxis: {
+      type: 'log',
+      autorange: true,
+      title: 'Performance (GFlops/s)',
+    },
   };
 
-  let data = [trace1, trace2];
-  
-  console.log('drawing the plot...');
+  Plotly.newPlot('roofline-plot', data, layout);
 
-  Plotly.newPlot('roofline-plot', data);
-  
-  console.log('plot drawn');
 }
 
 window.addEventListener('DOMContentLoaded', drawRooflinePlot, false);
